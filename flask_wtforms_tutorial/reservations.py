61226687f36bd2551ae593db7ba74f81c69af1d1
PRunner77 <pjmddc@mail.missouri.edu>
2021-05-04 18:07:28 -0500

    Finsihed functionality of web application

--- flask_wtforms_tutorial/reservations.py
+++ flask_wtforms_tutorial/reservations.py
@@ -19,16 +19,43 @@ seatingChart = [["O","O","O","O"],
 def get_cost_matrix():
     cost_matrix = [[100, 75, 50, 100] for row in range(12)]
     return cost_matrix
+    
+
+def get_total():
+    costChart = get_cost_matrix()
+    reservations = open("/project/reservations.txt", "r")
+    totalSales = 0
+    for cash in reservations:
+        line = cash.split(",")
+        seat = int(line[2])
+        row = int(line[1])
+        seatingChart[row][seat] = "X"
+               
+        if seatingChart[row][seat] == "X":
+            sale = costChart[row][seat]
+            totalSales += sale
+    reservations.close() 
+    
+    return totalSales
+    
+
 def Admin(username, password):
     textFile = open("/project/passcodes.txt", "r")
-
+   
+    error = "ERROR: The username/password was incorrect!"
+    
     userCode = []
     passCode = []
     for code in textFile:
-        line = code.split
-        userCode.append(line[0])
-        passCode.append(line[1])
-        
+        line = code.split(',')
+        userCode.append(str(line[0]))
+        passCode.append(str(line[1]).replace("\n",""))
+   
+
+    if username in userCode and password in passCode:
+        return None
+    else:
+        return error
         
 
 def GenerateChart():
@@ -36,9 +63,9 @@ def GenerateChart():
     seatingChartRevised = []
     for person in text:
         line = person.split(',')
-        seat = int(line[1])
-        row = int(line[2])
-        seatingChart[seat][row] = "X"
+        seat = int(line[2])
+        row = int(line[1])
+        seatingChart[row][seat] = "X"
     with open('/project/chart.txt', 'w') as writeFile:
         for x in seatingChart:
             writeFile.write(' '.join([str(a) for a in x]) + '\n')
@@ -51,25 +78,24 @@ def imageChart():
     content_split = content.splitlines()
     return content_split
 
-def Reservations(row, seat):
-    rowActual = int(row) + int(1)
-    seatActual = int(seat) + int(1)
-    ErrorSeatTaken = "ERROR: row " + str(seatActual) + " seat " + str(rowActual) + " is already taken, please choose another"
-    ErrorNameToLong = "ERROR: Your name is to long"
+def Reservations(row, seat, rowActual, seatActual):
+    ErrorSeatTaken = "Row " + str(rowActual) + " seat " + str(seatActual) + " is already taken, please choose another"
+    ErrorNameToLong = "ERROR: Your name is too long."
     #check to see if seat already is taken
     seatTaken = open('/project/reservations.txt', 'r')
     for person in seatTaken:
         line = person.split(',')
-        seat = int(line[1])
-        row = int(line[2])
+        seatz = int(line[2])
+        rowz = int(line[1])
+        seatingChart[rowz][seatz] = "X"
     seatTaken.close()            
-    if seatingChart[seat][row] == "X":
+    if seatingChart[int(row)][int(seat)] == "X":
         return ErrorSeatTaken
     else: 
         return None
 
 #This is the function to write to the text file, no error checking here
-def ReservationsTest(firstName, row, seat):
+def ReservationsWrite(firstName, row, seat):
     #Noah and Shawn's development with the ticket
     flight = "INFOTC4320"
     ticket = "".join([firstName[i] + flight[i] for i in range(len(firstName))]) + flight[len(firstName):]
@@ -78,3 +104,9 @@ def ReservationsTest(firstName, row, seat):
     new_seat = "\n" + firstName+", "+row+", "+seat + ", "+ticket
     text.write(new_seat)
     text.close()
+
+def Successful(rowActual, seatActual, firstName):
+    flight = "INFOTC4320"
+    ticket = "".join([firstName[i] + flight[i] for i in range(len(firstName))]) + flight[len(firstName):]
+    success = "Congratulations " + firstName + "! Row " + str(rowActual) + " Seat " + str(seatActual) + " is now reserved for you! Enjoy the Trip! Your e-ticket number is " + ticket
+    return success
--- flask_wtforms_tutorial/routes.py
+++ flask_wtforms_tutorial/routes.py
@@ -25,12 +25,32 @@ def user_options():
 def admin():
 
     form = AdminLoginForm()
+    err = None
+    success = None
+    chart = None
+    total = None
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            #Get the form data to query the api
+            userName = request.form['username']
+            passWord = request.form['password']
+            
 
-    return render_template("admin.html", form=form, template="form-template")
+            #if the reservations includes no errors, write to the text file
+            if Admin(userName, passWord) == None:
+                GenerateChart() # This writes to chart.txt the current reservations
+                chart = imageChart() #This reads the text file and splits line by line
+                total = get_total()
+            else:
+                
+                err = Admin(userName, passWord)
+
+    return render_template("admin.html", form=form, template="form-template",err = err, chart=chart, total=total)
 
 @app.route("/reservations", methods=['GET', 'POST'])
 def reservations():
     err = None
+    success = None
     form = ReservationForm()
     if request.method == 'POST':
         if form.validate_on_submit():
@@ -38,17 +58,23 @@ def reservations():
             firstName = request.form['first_name']
             lastName = request.form['last_name']
             row = request.form['row']#this is the row
+            rowActual = int(row) + 1
             seat = request.form['seat']#this is the column
+            seatActual = int(seat) + 1
 
-            if Reservations(seat, row) == None:
-                ReservationsTest(firstName, row, seat)
+            #if the reservations includes no errors, write to the text file
+            if Reservations(row, seat, rowActual, seatActual) == None:
+                ReservationsWrite(firstName, row, seat)
+                success = Successful(rowActual, seatActual, firstName)
+                
             else:
-                err = Reservations(seat, row)
+                
+                err = Reservations(row, seat, rowActual, seatActual)
 #Return the chart here the things above will be passed to the Generat Chart or maybe another method
     GenerateChart() # This writes to chart.txt the current reservations
 
     chart = imageChart() #This reads the text file and splits line by line
 
 
-    return render_template("reservations.html", form=form, template="form-template", err = err, chart=chart) #add the chart = chart and error = error
+    return render_template("reservations.html", form=form, template="form-template", err = err, chart=chart, success = success) #add the chart = chart and error = error
 
